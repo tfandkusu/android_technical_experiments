8db7665d43cb766723bf7c5da6cdcfbd5de6f69d
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-09-17 03:47:49 +0900

    Make MainActivity to select experiments

--- app/src/main/java/com/tfandkusu/ate/feature/main/MainActivity.kt
+++ app/src/main/java/com/tfandkusu/ate/feature/main/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.tfandkusu.ate
+package com.tfandkusu.ate.feature.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,10 +12,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.tfandkusu.ate.R
 import com.tfandkusu.ate.component.MyTopAppBar
+import com.tfandkusu.ate.feature.jnta.JetpackNavigationTransitionAnimationActivity
 import com.tfandkusu.ate.theme.MyTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,17 +40,31 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize(),
                         contentPadding = innerPadding,
                     ) {
-                        items(20) {
-                            Text(
-                                text = "Item $it",
+                        item {
+                            TextButton(
                                 modifier = Modifier
                                     .fillMaxWidth()
                                     .padding(16.dp),
-                            )
+                                onClick = { callJetpackNavigationTransitionAnimationActivity() },
+                            ) {
+                                Text(
+                                    text = stringResource(
+                                        R.string.jetpack_navigation_transition_animation,
+                                    ),
+                                )
+                            }
                         }
                     }
                 }
             }
         }
+    }
+
+    private fun callJetpackNavigationTransitionAnimationActivity() {
+        val intent = Intent(
+            this,
+            JetpackNavigationTransitionAnimationActivity::class.java,
+        )
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/tfandkusu/ate/navigation/JetpackNavigationTransitionAnimationActivityAlias.kt
@@ -0,0 +1,3 @@
+package com.tfandkusu.ate.navigation
+
+sealed class JetpackNavigationTransitionAnimationActivityAlias
