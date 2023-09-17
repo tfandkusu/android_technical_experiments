81a3a861bc25c29b251eeeab561e7086288f6086
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-09-17 19:02:35 +0900

    Set up Jetpack Navigation

--- app/src/main/java/com/tfandkusu/ate/feature/jnta/JetpackNavigationTransitionAnimationActivity.kt
+++ app/src/main/java/com/tfandkusu/ate/feature/jnta/JetpackNavigationTransitionAnimationActivity.kt
@@ -1,12 +1,14 @@
 package com.tfandkusu.ate.feature.jnta
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.fragment.app.FragmentActivity
+import com.tfandkusu.ate.R
 
-class JetpackNavigationTransitionAnimationActivity : ComponentActivity() {
+class JetpackNavigationTransitionAnimationActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContentView(R.layout.activity_jetpack_navigation_transition_animation)
     }
 }
---
+++ app/src/main/java/com/tfandkusu/ate/feature/jnta/No1Fragment.kt
@@ -0,0 +1,74 @@
+package com.tfandkusu.ate.feature.jnta
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import com.tfandkusu.ate.R
+import com.tfandkusu.ate.component.MyTopAppBar
+import com.tfandkusu.ate.theme.MyTheme
+
+class No1Fragment : Fragment() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(
+                ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed,
+            )
+            setContent {
+                MyTheme {
+                    Scaffold(
+                        topBar = {
+                            MyTopAppBar(
+                                title = {
+                                    Text(text = stringResource(id = R.string.screen_n, 1))
+                                },
+                                hasBack = true,
+                                onBackPressed = {
+                                    requireActivity().finish()
+                                },
+                            )
+                        },
+                    ) { innerPadding ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                                .padding(16.dp),
+                            verticalArrangement = spacedBy(16.dp),
+                        ) {
+                            Button(
+                                onClick = { },
+                            ) {
+                                Text(text = stringResource(id = R.string.open_screen_n, 2))
+                            }
+                            Button(
+                                onClick = { },
+                            ) {
+                                Text(text = stringResource(id = R.string.open_screen_n, 3))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
