a0c1f37ef2eb2a5455af0d46abfc54cc2f59190e
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-09-18 00:43:23 +0900

    Add No2Fragment

--- app/src/main/java/com/tfandkusu/ate/feature/jnta/No2Fragment.kt
+++ app/src/main/java/com/tfandkusu/ate/feature/jnta/No2Fragment.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,7 +19,7 @@ import com.tfandkusu.ate.R
 import com.tfandkusu.ate.component.MyTopAppBar
 import com.tfandkusu.ate.theme.MyTheme
 
-class No1Fragment : Fragment() {
+class No2Fragment : Fragment() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,7 +36,7 @@ class No1Fragment : Fragment() {
                         topBar = {
                             MyTopAppBar(
                                 title = {
-                                    Text(text = stringResource(id = R.string.screen_n, 1))
+                                    Text(text = stringResource(id = R.string.screen_n, 2))
                                 },
                                 hasBack = true,
                                 onBackPressed = {
@@ -48,29 +45,13 @@ class No1Fragment : Fragment() {
                             )
                         },
                     ) { innerPadding ->
-                        Column(
+                        Text(
                             modifier = Modifier
                                 .fillMaxSize()
                                 .padding(innerPadding)
                                 .padding(16.dp),
-                            verticalArrangement = spacedBy(16.dp),
-                        ) {
-                            Button(
-                                onClick = {
-
-//                                    findNavController().navigate(
-//
-//                                    )
-                                },
-                            ) {
-                                Text(text = stringResource(id = R.string.open_screen_n, 2))
-                            }
-                            Button(
-                                onClick = { },
-                            ) {
-                                Text(text = stringResource(id = R.string.open_screen_n, 3))
-                            }
-                        }
+                            text = stringResource(id = R.string.screen_n, 2)
+                        )
                     }
                 }
             }
