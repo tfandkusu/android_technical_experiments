24e8eac379b470d4929e29e474e999ee42470f59
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-12-10 05:07:12 +0900

    Add supporting text

--- app/src/main/java/com/tfandkusu/ate/feature/keyboard/SoftKeyboardExperimentScreen.kt
+++ app/src/main/java/com/tfandkusu/ate/feature/keyboard/SoftKeyboardExperimentScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,6 +22,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.tfandkusu.ate.R
 import com.tfandkusu.ate.component.MyTopAppBar
@@ -79,12 +81,13 @@ fun SoftKeyboardExperimentScreen(
                             text = it
                         },
                         maxLines = 1,
-                    )
-                }
-                item {
-                    Text(
-                        modifier = Modifier.padding(horizontal = 16.dp),
-                        text = stringResource(R.string.supporting_text),
+                        supportingText = {
+                            Text(
+                                modifier = Modifier.fillMaxWidth(),
+                                textAlign = TextAlign.End,
+                                style = MaterialTheme.typography.bodyMedium,
+                                text = stringResource(R.string.supporting_text))
+                        }
                     )
                 }
             }
