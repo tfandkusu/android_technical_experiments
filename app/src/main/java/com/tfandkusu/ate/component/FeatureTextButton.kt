4d6455cfd23b420d0ac5c019a9abad145b15ff45
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-12-10 03:47:56 +0900

    Make feature text button composable

---
+++ app/src/main/java/com/tfandkusu/ate/component/FeatureTextButton.kt
@@ -0,0 +1,30 @@
+package com.tfandkusu.ate.component
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FeatureTextButton(
+    @StringRes nameStringResId: Int,
+    onClick: () -> Unit,
+) {
+    TextButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        onClick = onClick,
+    ) {
+        Text(
+            text = stringResource(
+                nameStringResId,
+            ),
+        )
+    }
+}
--- app/src/main/java/com/tfandkusu/ate/feature/main/MainActivity.kt
+++ app/src/main/java/com/tfandkusu/ate/feature/main/MainActivity.kt
@@ -6,17 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.tfandkusu.ate.R
+import com.tfandkusu.ate.component.FeatureTextButton
 import com.tfandkusu.ate.component.MyTopAppBar
 import com.tfandkusu.ate.feature.jnta.JetpackNavigationTransitionAnimationActivity
 import com.tfandkusu.ate.theme.MyTheme
@@ -41,18 +38,12 @@ class MainActivity : ComponentActivity() {
                         contentPadding = innerPadding,
                     ) {
                         item {
-                            TextButton(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp),
-                                onClick = { callJetpackNavigationTransitionAnimationActivity() },
-                            ) {
-                                Text(
-                                    text = stringResource(
-                                        R.string.jetpack_navigation_transition_animation,
-                                    ),
-                                )
-                            }
+                            FeatureTextButton(
+                                nameStringResId = R.string.jetpack_navigation_transition_animation,
+                                onClick = {
+                                    callJetpackNavigationTransitionAnimationActivity()
+                                },
+                            )
                         }
                     }
                 }
