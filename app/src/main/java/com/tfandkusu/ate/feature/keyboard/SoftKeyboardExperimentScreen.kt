dffb12f3383116bdb178e349da5a6e6c5aca2bd6
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-12-10 04:01:42 +0900

    Add TextField

--- app/src/main/java/com/tfandkusu/ate/feature/keyboard/SoftKeyboardExperimentScreen.kt
+++ app/src/main/java/com/tfandkusu/ate/feature/keyboard/SoftKeyboardExperimentScreen.kt
@@ -1,13 +1,19 @@
 package com.tfandkusu.ate.feature.keyboard
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -32,13 +38,22 @@ fun SoftKeyboardExperimentScreen(
                 )
             },
         ) { innerPadding ->
-            Column(
+            var text by remember { mutableStateOf("") }
+            LazyColumn(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
                     .padding(16.dp),
                 verticalArrangement = Arrangement.Absolute.spacedBy(16.dp),
             ) {
+                item {
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = text, onValueChange = {
+                            text = it
+                        }, maxLines = 1
+                    )
+                }
             }
         }
     }
